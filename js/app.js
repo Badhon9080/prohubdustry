78bd2202ce3aa2b248cd1ef2ca96138b4d5e7a79
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2023-08-13 17:30:43 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//SJSVBMNLOVE!!!!!!11!!!!!
+$('.count-start').counterUp({ delay: 20,
+    time: 2000,
+})
+$('.banner_slider').slick({
+    prevArrow:'<i class="fa-solid fa-chevron-left banner_slick_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right banner_slick_arrow"></i>',
+    dots:true,
+    dotsClass:'banner_slider_dots',
+    /*fade: true,
+    autoplay: true,
+    autoplaySpeed:1000,
+    speed:1000,*/
+ });
